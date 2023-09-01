b1daf536db6435ede177d344f9654db33baadb63
gsofter <real.kingstar@outlook.com>
2023-08-31 21:56:19 -0500

    add amazon native ads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Head from 'next/head';
-import * as React from 'react';
+import React, { useEffect } from 'react';
 
 import ArrowLink from '@/components/links/ArrowLink';
 import ButtonLink from '@/components/links/ButtonLink';
@@ -21,12 +21,44 @@ import Logo from '~/svg/Logo.svg';
 // Before you begin editing, follow all comments with `STARTERCONF`,
 // to customize the default configuration.
 
+const HeaderScript = () => {
+  useEffect(() => {
+    // Create and append the Amazon item script
+    const script = document.createElement('script');
+    script.innerHTML = `
+        amzn_assoc_tracking_id = "theprose-20";
+        amzn_assoc_ad_mode = "manual";
+        amzn_assoc_ad_type = "smart";
+        amzn_assoc_marketplace = "amazon";
+        amzn_assoc_region = "US";
+        amzn_assoc_design = "enhanced_links";
+        amzn_assoc_asins = "B09NFNWQ18";
+        amzn_assoc_placement = "adunit";
+        amzn_assoc_linkid = "6e5e892e42d1b382b1d32da61fcb1511";
+      `;
+    document.head.appendChild(script);
+
+    // Load the Amazon ad script
+    const amazonAdScript = document.createElement('script');
+    amazonAdScript.src =
+      '//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US';
+    amazonAdScript.async = true;
+    document.head.appendChild(amazonAdScript);
+
+    // Attach click event listener to the Amazon item
+  }, []);
+
+  return <></>;
+};
+
 export default function HomePage() {
   return (
     <main>
       <Head>
         <title>Hi</title>
       </Head>
+      <div id='6e5e892e42d1b382b1d32da61fcb1511'></div>
+      <HeaderScript />
       <section className='bg-white'>
         <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
           <Logo className='w-16' />
